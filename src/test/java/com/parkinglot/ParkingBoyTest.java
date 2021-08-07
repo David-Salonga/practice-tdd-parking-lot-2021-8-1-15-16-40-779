5e01ac989aa218e57c7887b94a29e6af3b8800a3
David Salonga <fdc.salonga@gmail.com>
2021-08-07 18:49:34 +0800

    [ParkingBoy][Francis] feat: implement should_return_parking_ticket_when_park_given_a_parking_lot_standard_parking_boy_and_a_car

--- src/test/java/com/parkinglot/ParkingBoyTest.java
+++ src/test/java/com/parkinglot/ParkingBoyTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ParkingBoyTest {
     @Test
-    void should_return_parking_ticket_when_park_given_a_parking_lot_standard_parking_boy_and_a_car() {
+        void should_return_parking_ticket_when_park_given_a_parking_lot_standard_parking_boy_and_a_car() {
         //given
         ParkingLot parkingLot = new ParkingLot();
         ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
