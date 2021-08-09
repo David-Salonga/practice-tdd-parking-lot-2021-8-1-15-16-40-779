196eca9595d6458799dabee0867083fd25a999d4
David Salonga <fdc.salonga@gmail.com>
2021-08-09 20:48:55 +0800

    [ST5-SmartParkingBoy][Francis] test & feat: should_return_nothing_with_error_message_unrecognized_parking_ticket_when_fetch_given_smart_parking_boy_who_manage_two_parking_lots_and_an_unrecognized_ticket

--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SmartParkingBoyTest {
 
@@ -72,5 +73,19 @@ public class SmartParkingBoyTest {
         assertEquals(firstCar, actualFirstCar);
         assertEquals(secondCar, actualSecondCar);
     }
-
+    @Test
+    public void should_return_nothing_with_error_message_unrecognized_parking_ticket_when_fetch_given_smart_parking_boy_who_manage_two_parking_lots_and_an_unrecognized_ticket() {
+        //given
+        ParkingLot parkingLot1 = new ParkingLot();
+        ParkingLot parkingLot2 = new ParkingLot();
+        ArrayList<ParkingLot> parkingLots = new ArrayList<>();
+        parkingLots.add(parkingLot1);
+        parkingLots.add(parkingLot2);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+        ParkingTicket unrecognizedParkingTicket = new ParkingTicket();
+        //when
+        Exception exception = assertThrows(UnrecognizedParkingTicketException.class, () -> smartParkingBoy.fetch(unrecognizedParkingTicket));
+        //then
+        assertEquals("Unrecognized parking ticket.", exception.getMessage());
+    }
 }
