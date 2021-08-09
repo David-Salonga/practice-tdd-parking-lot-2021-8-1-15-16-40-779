7ac279e45d3c92397b4e8ca3e72986cc1368be0f
David Salonga <fdc.salonga@gmail.com>
2021-08-09 20:50:53 +0800

    [ST5-SmartParkingBoy][Francis] test: renamed standard to smart

--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class SmartParkingBoyTest {
 
     @Test
-    void should_park_car_to_the_first_parking_lot_when_park_given_standard_parking_boy_two_parking_lots_with_available_positions_and_car() {
+    void should_park_car_to_the_first_parking_lot_when_park_given_smart_parking_boy_two_parking_lots_with_available_positions_and_car() {
         //given
         ParkingLot parkingLot1 = new ParkingLot();
         ParkingLot parkingLot2 = new ParkingLot();
@@ -30,7 +30,7 @@ public class SmartParkingBoyTest {
     }
 
     @Test
-    void should_park_car_to_the_second_parking_lot_when_park_given_standard_parking_boy_two_parking_lots_with_first_parking_lot_full_second_parking_lot_available_position_and_car() {
+    void should_park_car_to_the_second_parking_lot_when_park_given_smart_parking_boy_two_parking_lots_with_first_parking_lot_full_second_parking_lot_available_position_and_car() {
         //given
         Car car = new Car();
         ParkingLot parkingLot1 = new ParkingLot(3);
@@ -50,7 +50,7 @@ public class SmartParkingBoyTest {
     }
 
     @Test
-    void should_return_right_car_with_each_ticket_when_fetch_twice_given_standard_parking_boy_two_parking_lots_both_with_a_parked_car_lot_and_two_parking_tickets() {
+    void should_return_right_car_with_each_ticket_when_fetch_twice_given_smart_parking_boy_two_parking_lots_both_with_a_parked_car_lot_and_two_parking_tickets() {
         //given
         List<ParkingLot> parkingLots = new ArrayList<>();
         ParkingLot parkingLot1 = new ParkingLot();
