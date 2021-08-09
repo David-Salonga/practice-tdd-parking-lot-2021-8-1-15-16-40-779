ac81a1b3a32ebe1dcdc64268ac6a2a9c2b7a2cf3
David Salonga <fdc.salonga@gmail.com>
2021-08-09 22:27:36 +0800

    [ST6-SuperSmartParkingBoy][Francis] test & feat: should_park_car_to_the_first_parking_lot_when_park_given_super_smart_parking_boy_and_two_parking_lots_with_available_positions_and_car

---
+++ src/test/java/com/parkinglot/SuperSmartParkingBoyTest.java
@@ -0,0 +1,29 @@
+package com.parkinglot;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SuperSmartParkingBoyTest {
+    @Test
+    void should_park_car_to_the_first_parking_lot_when_park_given_super_smart_parking_boy_and_two_parking_lots_with_available_positions_and_car() {
+        //given
+        ParkingLot parkingLot1 = new ParkingLot();
+        ParkingLot parkingLot2 = new ParkingLot();
+        List<ParkingLot> parkingLots = new ArrayList<>();
+        SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy(parkingLots);
+        parkingLots.add(parkingLot1);
+        parkingLots.add(parkingLot2);
+        Car car = new Car();
+
+        //when
+        ParkingTicket parkingTicket = superSmartParkingBoy.park(car);
+
+        //then
+        Car actualCar = parkingLot1.fetch(parkingTicket);
+        assertEquals(car, actualCar);
+    }
+}
